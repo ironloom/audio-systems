25800e0bd947311efceba466b0926f6d15f6d625
zewenn <takaczsoli@gmail.com>
2025-04-01 20:55:07 +0200

    new: libsoundio

--- build.zig
+++ build.zig
@@ -1,5 +1,15 @@
 const std = @import("std");
 
+const ALLOCATOR = std.heap.smp_allocator;
+
+inline fn fromRoot(comptime str: []const u8) []const u8 {
+    return (comptime std.fs.path.dirname(@src().file) orelse ".") ++ str;
+}
+
+inline fn path(comptime str: []const u8) std.Build.LazyPath {
+    return std.Build.LazyPath{ .cwd_relative = "." ++ str };
+}
+
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -8,18 +18,77 @@ pub fn build(b: *std.Build) !void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
+
+    const cflags = [_][]const u8{
+        "-std=c11",
+        "-fvisibility=hidden",
+        "-Werror=strict-prototypes",
+        "-Werror=old-style-definition",
+        "-Werror=missing-prototypes",
+        "-Wno-nullability-extension",
+        "-D_REENTRANT",
+        "-D_POSIX_C_SOURCE=200809L",
+        "-Wno-missing-braces",
+        "-Wno-deprecated-declarations",
+        "-Wno-unused-variable",
+        "-Wno-unused-but-set-variable",
+    };
+    const csources = [_][]const u8{
+        fromRoot("/libsoundio/src/soundio.c"),
+        fromRoot("/libsoundio/src/util.c"),
+        fromRoot("/libsoundio/src/os.c"),
+        fromRoot("/libsoundio/src/dummy.c"),
+        fromRoot("/libsoundio/src/channel_layout.c"),
+        fromRoot("/libsoundio/src/ring_buffer.c"),
+    };
+
+    lib_mod.addConfigHeader(b.addConfigHeader(
+        .{
+            .include_path = "config.h",
+            .style = .{
+                .cmake = path("/libsoundio/src/config.h.in"),
+            },
+        },
+        .{
+            .LIBSOUNDIO_VERSION_MAJOR = "2",
+            .LIBSOUNDIO_VERSION_MINOR = "0",
+            .LIBSOUNDIO_VERSION_PATCH = "0",
+            .LIBSOUNDIO_VERSION = "2.0.0",
+        },
+    ));
+
+    lib_mod.addIncludePath(path("/libsoundio"));
+    lib_mod.addIncludePath(path("/libsoundio/src"));
+    lib_mod.addCSourceFiles(.{
+        .flags = &cflags,
+        .files = &csources,
+    });
+
+    lib_mod.addCMacro("ZIG_BUILD", "NULL");
 
     if (b.lazyDependency("system_sdk", .{})) |system_sdk| switch (target.result.os.tag) {
         .windows => if (target.result.cpu.arch.isX86() and (target.result.abi.isGnu() or target.result.abi.isMusl())) {
             lib_mod.addLibraryPath(system_sdk.path("windows/lib/x86_64-windows-gnu"));
+            lib_mod.linkSystemLibrary("ole32", .{ .needed = true });
+
+            lib_mod.addCMacro("SOUNDIO_HAVE_WASAPI", "NULL");
+            lib_mod.addCSourceFile(.{ .file = path("/libsoundio/src/wasapi.c") });
         },
         .macos => {
             lib_mod.addLibraryPath(system_sdk.path("macos12/usr/lib"));
             lib_mod.addFrameworkPath(system_sdk.path("macos12/System/Library/Frameworks"));
 
             lib_mod.linkFramework("CoreFoundation", .{ .needed = true });
+            lib_mod.linkFramework("CoreAudio", .{ .needed = true });
+            lib_mod.linkFramework("AudioUnit", .{ .needed = true });
             lib_mod.linkFramework("AudioToolbox", .{ .needed = true });
+
+            lib_mod.addCMacro("SOUNDIO_HAVE_COREAUDIO", "NULL");
+            lib_mod.addCSourceFile(.{
+                .file = path("/libsoundio/src/coreaudio.c"),
+            });
         },
         .linux => {
             if (target.result.cpu.arch.isX86()) {
@@ -27,6 +96,9 @@ pub fn build(b: *std.Build) !void {
             } else if (target.result.cpu.arch == .aarch64) {
                 lib_mod.addLibraryPath(system_sdk.path("linux/lib/aarch64-linux-gnu"));
             }
+
+            lib_mod.addCMacro("SOUNDIO_HAVE_ALSA", "NULL");
+            lib_mod.addCSourceFile(.{ .file = path("/libsoundio/src/alsa.c") });
         },
         else => {},
     };
