bf49f7b4ba866166623f6e8fa3c4f8be831f60f4
zewenn <takaczsoli@gmail.com>
2025-04-02 13:15:06 +0200

    add: ported libsoundio example to zig

--- build.zig
+++ build.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 
-const ALLOCATOR = std.heap.smp_allocator;
-
 inline fn fromRoot(comptime str: []const u8) []const u8 {
     return (comptime std.fs.path.dirname(@src().file) orelse ".") ++ str;
 }
@@ -118,44 +116,21 @@ pub fn build(b: *std.Build) !void {
         .name = "audio_systems",
         .root_module = lib_mod,
     });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 
-    // This creates another `std.Build.Step.Compile`, but this one builds an executable
-    // rather than a static library.
     const exe = b.addExecutable(.{
         .name = "audio_systems",
         .root_module = exe_mod,
     });
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
 
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,121 @@
 const std = @import("std");
 const c = @cImport({
     @cInclude("soundio/soundio.h");
+    @cInclude("stdlib.h");
 });
 
+inline fn NULL(comptime T: type) T {
+    return @ptrFromInt(@as(c_int, 0));
+}
+inline fn isNull(ptr: anytype) bool {
+    return switch (@typeInfo(@TypeOf(ptr))) {
+        .pointer => @intFromPtr(ptr) == 0,
+        .null => true,
+        else => false,
+    };
+}
+const PI = std.math.pi;
+var seconds_offset: f32 = 0.0;
+
+inline fn handleError(err: c_int, comptime meassage: []const u8) void {
+    if (err == 0) return;
+    std.log.info("error code: {d}", .{err});
+    std.log.info(" - soundIO message: {s}", .{c.soundio_strerror(err)});
+    std.log.info(" - user message: {s}", .{meassage});
+    std.process.exit(1);
+}
+
+fn writeCallback(outstream: [*c]c.SoundIoOutStream, frame_count_min: c_int, frame_count_max: c_int) callconv(.c) void {
+    _ = frame_count_min;
+
+    const layout: [*c]c.SoundIoChannelLayout = &(outstream.?.*.layout);
+    const float_sample_rate: f32 = @floatFromInt(outstream.?.*.sample_rate);
+    const seconds_per_frame: f32 = 1 / float_sample_rate;
+
+    var areas = NULL([*c]c.SoundIoChannelArea);
+    var frames_left = frame_count_max;
+
+    while (frames_left > 0) {
+        var frame_count = frames_left;
+
+        handleError(
+            c.soundio_outstream_begin_write(
+                outstream,
+                &areas,
+                &frame_count,
+            ),
+            "Outstream begin write failed",
+        );
+
+        if (frame_count == 0) break;
+
+        const pitch: comptime_float = 440.0;
+        const radians_per_second: comptime_float = pitch * 2.0 * PI;
+
+        for (0..@intCast(frame_count)) |frame| {
+            const sample = std.math.sin((seconds_offset + @as(f32, @floatFromInt(frame)) * seconds_per_frame) * radians_per_second);
+            for (0..@intCast(layout.?.*.channel_count)) |channel| {
+                const ptr: [*c]f32 =
+                    @ptrFromInt(@intFromPtr(areas[channel].ptr) + @as(usize, @intCast(areas[channel].step)) * frame);
+                ptr.* = sample;
+            }
+        }
+
+        seconds_offset = @rem(seconds_offset + seconds_per_frame * @as(f32, @floatFromInt(frame_count)), @as(comptime_float, 1.0));
+
+        handleError(
+            c.soundio_outstream_end_write(outstream),
+            "Outstream write end failed!",
+        );
+
+        frames_left -= frame_count;
+    }
+}
+
 pub fn main() !void {
-    std.log.info("soundio version: {s}", .{c.soundio_version_string()});
+    const soundIO = c.soundio_create();
+    defer c.soundio_destroy(soundIO);
+
+    handleError(
+        c.soundio_connect(soundIO),
+        "Failed to connect soundIO",
+    );
+
+    c.soundio_flush_events(soundIO);
+
+    const default_device_index = c.soundio_default_output_device_index(soundIO);
+    if (default_device_index < 0) {
+        std.log.err("No device found", .{});
+        return;
+    }
+
+    const device = c.soundio_get_output_device(soundIO, default_device_index);
+    if (isNull(device)) {
+        std.log.err("Out of memory", .{});
+        return;
+    }
+
+    std.log.debug("Output device: {s}", .{device.?.*.name});
+    const outstream = c.soundio_outstream_create(device);
+    outstream.?.*.format = c.SoundIoFormatFloat32NE;
+    outstream.?.*.write_callback = writeCallback;
+
+    handleError(
+        c.soundio_outstream_open(outstream),
+        "Failed to open outstream",
+    );
+
+    if (outstream.?.*.layout_error != 0)
+        std.log.err("unable to set channel layout: {s}", .{c.soundio_strerror(outstream.?.*.layout_error)});
+
+    handleError(
+        c.soundio_outstream_start(outstream),
+        "Failed to start outstream",
+    );
+
+    while (true) {
+        c.soundio_wait_events(soundIO);
+    }
+
+    defer c.soundio_device_unref(device);
 }
